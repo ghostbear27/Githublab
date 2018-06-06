7329af14b2748c211b115135d117ed6db2e5d4cc
Sahaj <sahaj@getreveel.com>
2018-06-05 20:40:01 -0500

    added variable Basket, added function fruit_basket

---
+++ fruitbasket.py
@@ -0,0 +1,6 @@
+def guess_fruit( fruit, basket_type):
+	# def guess_fuit():        is defining a function
+	print(fruit + basket_type)
+	print(fruit + " aint nobodys business and lives in the " + basket_type + " basket")
+ 
+guess_fruit("MANGO", "Weave")   # is calling a function
